1d5afa2175b50d93bcb1d4adcf00e1b460d3222e
EMPAJANY <EMPAJANY@LFR203735.corp.capgemini.com>
2017-05-19 15:41:01 +0200

    1er Hello

---
+++ src/helloJenkins/helloJenkins.java
@@ -0,0 +1,10 @@
+package helloJenkins;
+
+public class helloJenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Coucou les amis");
+	}
+
+}
